615bae01462fc1988a34d2780947ff7477b4b4c0
hustcalm <licalmer@gmail.com>
2013-01-08 00:36:41 +0800

    实现了历史的我UI的基本框架，接下来需要显示具体内容

--- src/com/calm/lifemanager/MonthlyMoodHistoryActivity.java
+++ src/com/calm/lifemanager/MonthlyMoodHistoryActivity.java
@@ -1,12 +1,52 @@
 package com.calm.lifemanager;
 
 import android.app.Activity;
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TabHost;
 
-public class MonthlyMoodHistoryActivity extends Activity {
-	 @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_history_mood_monthly);
-	    }
+public class MonthlyMoodHistoryActivity extends TabActivity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        TabHost tabHost = getTabHost();
+        
+        LayoutInflater.from(this).inflate(R.layout.activity_history_mood_generic,tabHost.getTabContentView(),true);
+        
+		
+		TabView moodDistributeView = null;
+		moodDistributeView = new TabView(this,R.drawable.btn_efficient,R.drawable.btn_efficient);
+		moodDistributeView.setBackground(this.getResources().getDrawable(R.drawable.btn_efficient));
+		
+		TabView detailsView = null;
+		detailsView = new TabView(this,R.drawable.btn_sheet,R.drawable.btn_sheet);
+		detailsView.setBackground(this.getResources().getDrawable(R.drawable.btn_sheet));
+		
+        
+		tabHost.addTab(tabHost.newTabSpec("tab1")
+                .setIndicator(moodDistributeView)     
+                .setContent(R.id.activity_history_mood_daily_distribute));
+        
+        tabHost.addTab(tabHost.newTabSpec("tab2")
+                .setIndicator(detailsView)     
+                .setContent(R.id.activity_history_mood_daily_details));
+		
+		tabHost.setCurrentTab(0);  
+		
+		//Ìí¼Ó¼àÌýÊÂ¼þ  
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {  
+              
+            public void onTabChanged(String tabId) {  
+                if (tabId.equals("One")) {  
+                      
+                }  
+                if (tabId.equals("Two")) {  
+                    
+                }  
+            }  
+        });  
+          
+    }
 }

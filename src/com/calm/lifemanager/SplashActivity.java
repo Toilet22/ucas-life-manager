7eb29ecb4eeb6e80931c0625e858a0f1304f2883
hustcalm <licalmer@gmail.com>
2013-02-05 22:01:05 +0800

    初步的splash实现，用于展示产品logo、信息以及后台准备资源

---
+++ src/com/calm/lifemanager/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.calm.lifemanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+	private final int SPLASH_DISPLAY_LENGHT = 3000; // —”≥Ÿ»˝√Î
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		new Handler().postDelayed(new Runnable() {
+			public void run() {
+				Intent mainIntent = new Intent(SplashActivity.this,
+						MainActivity.class);
+				SplashActivity.this.startActivity(mainIntent);
+				SplashActivity.this.finish();
+			}
+
+		}, SPLASH_DISPLAY_LENGHT);
+
+	}
+}

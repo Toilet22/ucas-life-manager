978fa842a1cf7bc6f5e0752ada498b157c1333ec
FrankSu <fe22suyilun@gmail.com>
2013-02-22 00:59:01 +0800

    Toilet: 报错无法解决，请calm同学解决一下。

--- src/com/calm/lifemanager/PrimTypesActivity.java
+++ src/com/calm/lifemanager/PrimTypesActivity.java
@@ -1,10 +1,13 @@
 package com.calm.lifemanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class PrimTypesActivity extends Activity {
@@ -13,63 +16,61 @@ public class PrimTypesActivity extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);	
+
+        Log.i("PrimTypesActivity", "before setContentView");
+        setContentView(R.layout.activity_prim_types);
         
         //获取ListView实例
         Log.i("PrimTypesActivity","Get ListView");
-        lstvw_primTypes = new ListView(this);
+        lstvw_primTypes = (ListView)findViewById(R.id.act_primTypes_lstvw_primTypes);
+        
         //获取数据库操作单元
         dbUtil = new DatabaseUtil(this);
         Log.i("PrimTypesActivity","Open DB");
         dbUtil.open();
-        
-        Log.i("Insert","Create a user with specified User interface");
-        Cursor cursor;
-        String[] selectCol = {DatabaseUtil.KEY_TYPE_NAME, DatabaseUtil.KEY_TYPE_ICON};
+
         //获得指向数据的游标
+        final Cursor cursor;
+        //String[] selectCol = {DatabaseUtil.KEY_TYPE_NAME, DatabaseUtil.KEY_TYPE_ICON};
         //cursor = dbUtil.fetchAllData(DatabaseUtil.PRIM_TYPES, selectCol);
         cursor = dbUtil.rawQuery("SELECT DISTINCT oid as _id, type_name FROM tb_prim_types", null);
         
-        if(cursor != null) {
-        	while(cursor.moveToNext()) {
-        		Log.i("Toilet","PrimTypesActivity_fetchAllData: fetch specific data works!");
-        		Log.i("PrimTypes", " Type Name: " + cursor.getString(0)); 
-        		Log.i("PrimTypes", " Type Icon: " + cursor.getString(1));
-        	}  
-        	cursor.moveToFirst();
+        if(cursor.moveToNext()){
+        	Log.i("Toilet","PrimTypesActivity_fetchAllData: fetch specific data works!");
         }else{
         	Log.i("Toilet","PrimTypesActivity_fetchAllData: fetch specific data failed!");        	
         }        
          
-        /*
-        String[] selectColumns = {DatabaseUtil.KEY_USERNAME, DatabaseUtil.KEY_PASSWORD, DatabaseUtil.KEY_CTIME};
-        cursor = dbUtil.fetchAllData(DatabaseUtil.USER, selectColumns);
-        if(cursor != null && cursor.getCount() > 0) {
-        	Log.i("Database","cursor is not none.");
-        	
-        	while(cursor.moveToNext()) {
-        		Log.i("User", " User Name: " + cursor.getString(0) 
-        				+ " Password " + cursor.getString(1)
-        				+ " Create Time " + cursor.getString(2));
-        	}
-        }
-        */
-        
-        
         //获取SimpleCursorAdatper的实例
         String[] fromCol = {DatabaseUtil.KEY_TYPE_NAME};
         int[] toView = {R.id.layout_primType_name};
         @SuppressWarnings("deprecation")
 		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, 
         		R.layout.layout_prim_type, cursor, fromCol, toView);
-
-        Log.i("PrimTypesActivity", "after get SimpleCursorAdapter.");
-        
+        //绑定adapter
         Log.i("PrimTypesActivity", "before setAdatper");
         lstvw_primTypes.setAdapter(adapter);
         
+        lstvw_primTypes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-        Log.i("PrimTypesActivity", "before setContentView");
-        setContentView(R.layout.activity_prim_types);
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				// 获取被点击的类别名
+				cursor.moveToPosition(position);
+				String typeName = cursor.getString(1);
+				Log.i("cursor getString", typeName);
+				
+				//开启次级类别的Activity
+				Intent intent = new Intent(PrimTypesActivity.this, SubTypesActivity.class);
+				Bundle mBundle = new Bundle();
+				mBundle.putString("PrimTypeName", typeName);
+				intent.putExtras(mBundle);
+				startActivity(intent);
+			}
+        	
+		});
+        
+
 	}
 
 }
